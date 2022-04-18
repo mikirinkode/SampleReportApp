0fc4403fdedc62767d83344377f171ccef884629
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-04-18 14:52:08 +0700

    add crash testing

--- app/src/main/java/com/mikirinkode/myreportapp/MainActivity.kt
+++ app/src/main/java/com/mikirinkode/myreportapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.mikirinkode.myreportapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val crashButton = findViewById<Button>(R.id.btn_crash)
+        crashButton.setOnClickListener {
+            FirebaseCrashlytics.getInstance().log("Clicked on button")
+            FirebaseCrashlytics.getInstance().setCustomKey("str_key", "some_data")
+            throw RuntimeException("Test Crash")
+        }
     }
 }
